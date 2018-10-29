f3aa42044a10805f6c179ec261c32bf8350493ce
ZhangZisu <admin@zhangzisu.cn>
2018-10-29 18:26:05 +0800

    update

--- src/router.ts
+++ src/router.ts
@@ -3,8 +3,10 @@ import Router from "vue-router";
 import home from "./views/home.vue";
 import about from "./views/about.vue";
 import login from "./views/login.vue";
+import logout from "./views/logout.vue";
 import error from "./views/error.vue";
 import problemlist from "./views/problemlist.vue";
+import problemview from "./views/problemview.vue";
 import register from "./views/register.vue";
 
 Vue.use(Router);
@@ -29,6 +31,11 @@ export default new Router({
       component: login
     },
     {
+      path: "/logout",
+      name: "logout",
+      component: logout
+    },
+    {
       path: "/register",
       name: "register",
       component: register
@@ -37,6 +44,18 @@ export default new Router({
       path: "/problem",
       name: "problemListPrivate",
       component: problemlist
+    },
+    {
+      path: "/problem/:entry/:id",
+      name: "problemPrivateView",
+      component: problemview,
+      props: route => ({
+        url: "/api/private/problem",
+        query: {
+          entry: route.params.entry,
+          id: route.params.id
+        }
+      })
     },
     {
       path: "*",
