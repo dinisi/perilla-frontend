ce28f5c43db53b6139744cc0ee958fdda6e9464c
ZhangZisu <admin@zhangzisu.cn>
2018-11-27 18:17:57 +0800

    v0.3.6

--- src/components/dataedit.vue
+++ src/components/dataedit.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
     <div v-if="channel === 'bzoj' || channel === 'uoj' || channel === 'loj' || channel === 'hdu' || channel === 'poj'">
-      <v-text-field v-model="realval.id" :label="$t('id')" type="number"/>
+      <v-text-field v-model="remote_id" :label="$t('id')" type="number"/>
     </div>
-    <z-json-editor v-model="realval" v-else/>
+    <div v-else-if="channel === 'traditional'">
+      <input type="file" multiple ref="files">
+      <v-btn color="primary" @click="applyTrad" v-text="$t('apply')"/>
+    </div>
+    <z-json-editor v-model="realval"/>
   </div>
 </template>
 
 <script>
-import { deepCompare } from '@/utils'
+import { deepCompare, calcHash } from '@/utils'
 import zJsonEditor from '@/components/zjsoneditor.vue'
+import { request } from '@/http'
 
 export default {
   name: 'dataEdit',
@@ -25,7 +30,8 @@ export default {
     return {
       realval: {
         //
-      }
+      },
+      remote_id: null
     }
   },
   mounted () {
@@ -43,6 +49,86 @@ export default {
         this.realval = val || {}
       },
       deep: true
+    },
+    remote_id: function (val) {
+      val = parseInt(val)
+      if (typeof val === 'number') {
+        this.realval = { id: val }
+      }
+    }
+  },
+  methods: {
+    async applyTrad () {
+      if (this.$refs.files && this.$refs.files.files) {
+        const files = this.$refs.files.files
+        let map = new Map()
+        for (let file of files) {
+          let filename = file.name
+          filename = filename.substring(0, filename.lastIndexOf('.'))
+          if (!filename) continue
+          let ext = file.name.substr(filename.length + 1)
+          if (!ext) continue
+          console.log(filename)
+          console.log(ext)
+          map[filename] = map[filename] || {}
+          if (['in'].indexOf(ext)) {
+            map[filename].input = map[filename].input || file
+          }
+          if (['ans', 'out'].includes(ext)) {
+            map[filename].output = map[filename].output || file
+          }
+        }
+        let runcases = []
+        for (let key in map) {
+          if (map[key].input && map[key].output) {
+            runcases.push({
+              input: await this.upload(map[key].input),
+              output: await this.upload(map[key].output),
+              desc: key
+            })
+          }
+        }
+        this.realval = {
+          subtasks: [
+            {
+              name: 'Default',
+              timeLimit: 1,
+              memoryLimit: 524288,
+              score: 100,
+              runcases,
+              dependency: []
+            }
+          ]
+        }
+      }
+    },
+    async upload (file) {
+      this.$store.commit('updateMessage', this.$t('hashing', [file.name]))
+      const hash = await calcHash(file)
+      try {
+        await request({ url: '/api/file/provide', params: { hash } })
+        const form = new FormData()
+        form.append('file', file)
+        this.$store.commit('updateMessage', this.$t('uploading', [file.name]))
+        await request({
+          url: '/api/file/provide',
+          method: 'POST',
+          data: form,
+          onUploadProgress: e => {
+            this.currentProgress = Math.round((e.loaded * 100) / e.total)
+          }
+        })
+      } catch (e) {
+        console.log(e.message)
+      }
+      this.$store.commit('updateMessage', this.$t('creating', [file.name]))
+      const id = await request({
+        url: '/api/file',
+        params: { entry: this.$store.state.entry },
+        method: 'POST',
+        data: { hash, tags: ['TestData'], name: file.name }
+      })
+      return id
     }
   }
 }
