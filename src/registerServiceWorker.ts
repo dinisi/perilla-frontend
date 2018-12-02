790ae6250953243d7de12011147076d36bebf814
ZhangZisu <admin@zhangzisu.cn>
2018-12-02 20:28:32 +0800

    Add better support service-worker

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -1,25 +1,19 @@
-/* eslint-disable no-console */
-
 import { register } from 'register-service-worker'
+import store from './store'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-          'For more details, visit https://goo.gl/AFskqB'
-      )
+      store.commit('updateMessage', 'App is being served from cache by a service worker.')
     },
     cached () {
-      console.log('Content has been cached for offline use.')
+      store.commit('updateMessage', 'Content has been cached for offline use.')
     },
     updated () {
-      console.log('New content is available; please refresh.')
+      store.commit('updateMessage', 'New content is available; please refresh.')
     },
     offline () {
-      console.log(
-        'No internet connection found. App is running in offline mode.'
-      )
+      store.commit('updateMessage', 'No internet connection found. App is running in offline mode.')
     },
     error (error) {
       console.error('Error during service worker registration:', error)
