8759a40a4fb65ec03e1fc50e2a5ddeaec64ea7a6
ZhangZisu <admin@zhangzisu.cn>
2018-12-26 22:43:41 +0800

    v1.2.18

---
+++ src/components/hitokoto.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hitokoto">
+    <h1>“{{ content }}”</h1>
+    <blockquote>{{ from }}</blockquote>
+    <v-btn icon :loading="loading" @click="load"><v-icon>refresh</v-icon></v-btn>
+  </div>
+</template>
+
+<script>
+import { client } from '@/helpers/http'
+
+export default {
+  name: 'hitokoto',
+  data () {
+    return {
+      content: '一起闷声大发财，一起寿命都续掉',
+      from: '学蛤笑',
+      loading: false
+    }
+  },
+  created () {
+    this.load()
+  },
+  methods: {
+    async load () {
+      this.loading = true
+      try {
+        const res = await client.get('https://v1.hitokoto.cn', { params: { c: 'a' } })
+        this.content = res.data.hitokoto
+        this.from = res.data.from
+      } catch (e) {
+        this.content = this.$t('hitokoto_error')
+        this.from = e.message
+      }
+      this.loading = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.hitokoto
+  text-align end
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,17 +1,23 @@
 <template>
   <v-container fluid>
-    <v-layout row fill-height justify-center>
-      <img width="64px" height="64px" src="@/assets/logo.svg"/>
+    <v-layout row justify-space-between>
+      <v-flex sm3>
+        <img style="margin-left: auto; margin-right:auto; display:block;" width="64px" height="64px" src="@/assets/logo.svg"/>
+      </v-flex>
+      <v-flex sm9>
+        <hitokoto/>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import hitokoto from '@/components/hitokoto'
+
 export default {
-  data () {
-    return {
-      //
-    }
+  name: 'home',
+  components: {
+    hitokoto
   }
 }
 </script>
